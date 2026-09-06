cd3b888462b72db222309d3b165e16e4dbc7976d
MD Reduanul Hoque <mdreduanulhoquesadik@gmail.com>
2026-09-06 16:06:54 +0600

    Implemented errorHandler along with asyncHandler

--- Extra Learning/contack_project/controllers/contactController.js
+++ Extra Learning/contack_project/controllers/contactController.js
@@ -1,37 +1,48 @@
+const { request } = require("express");
+const asyncHandler = require("express-async-handler")
+
 // @dec Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = (req,res)=>{
+const getContacts = asyncHandler( async (req,res)=>{
     res.json({ message: "Get all contacts"});
-};
+});
 
 // @dec Create new contacts
 // @route POST /api/contacts
 // @access public
-const createContact = (req,res)=>{
+const createContact = asyncHandler( async (req,res)=>{
+    console.log(req.body);
+
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
     res.json({ message: "Create contacts"});
-};
+});
 
 // @dec Get contact by ID
 // @route GET /api/contacts/:id
 // @access public
-const getContact = (req,res)=>{
+const getContact = asyncHandler( async (req,res)=>{
     res.json({ message: `Get the contact for ${req.params.id}`});
-};
+});
 
 // @dec Update contact by ID
 // @route PUT /api/contacts/:id
 // @access public
-const updateCotact = (req,res)=>{
+const updateCotact = asyncHandler( async (req,res)=>{
     res.json({ message: `Update the contact for ${req.params.id}`});
-};
+});
 
 // @dec Delete contact by ID
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteCotact = (req,res)=>{
+const deleteCotact = asyncHandler( async (req,res)=>{
     res.json({ message: `Delete the contact for ${req.params.id}`});
-};
+});
 
 
 module.exports = {getContacts, createContact, getContact, updateCotact, deleteCotact};
--- Extra Learning/contack_project/server.js
+++ Extra Learning/contack_project/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(errorHandler);
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, ()=>{
